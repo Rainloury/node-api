edce474cbc21e8b942d10f216943f6b76132894f
liuyu <myhome51up@126.com>
2019-03-28 12:09:31 +0800

    init

---
+++ routes/api.js
@@ -0,0 +1,20 @@
+require("babel-register");
+const express = require("express");
+const router = express.Router();
+const https = require("https");
+const User = require('../models/User');
+
+//Api 
+
+router.get('/mockService/v1/query', (req, res, next) => {
+    const response = {
+        code: 0,
+        data: {
+            list: new User({
+                name: 'test'
+            })
+        }
+    }
+    res.send(JSON.stringify(response));
+})
+module.exports = router;
