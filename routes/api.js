ba2759411637776c636cceb2f6d2829dc583bbb3
liuyu <myhome51up@126.com>
2019-03-29 15:13:09 +0800

    add

--- routes/api.js
+++ routes/api.js
@@ -7,25 +7,30 @@ const User = require('../models/User');
 //Api 
 
 router.post('/mockService/v1/query', (req, res, next) => {
-    const response = {
-        code: 0,
-        data: {
-            list: new User({
-                name: 'test'
-            })
-        }
+  const data = (sum) => {
+    const temp = [];
+    var i = 0;
+    while (i < sum) {
+      temp.push(new User({
+        item: 'test'+ i
+      }));
+      i++;
     }
-    res.send(JSON.stringify(response));
+    return temp;
+  }
+  const response = {
+    code: 0,
+    data: data(10)
+  }
+  res.send(JSON.stringify(response));
 })
 router.post('/mockService/v1/detail', (req, res, next) => {
-    const response = {
-        code: 0,
-        data: {
-            list: new User({
-                name: 'detail'
-            })
-        }
-    }
-    res.send(JSON.stringify(response));
+  const response = {
+    code: 0,
+    data: new User({
+      name: 'detail'
+    })
+  }
+  res.send(JSON.stringify(response));
 })
 module.exports = router;
--- schemas/DetailSchema.js
+++ schemas/DetailSchema.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 //创建Schema
-var userSchema = new Schema({
-  name: String
+var detailSchema = new Schema({
+  item: String
 })
 
-module.exports = userSchema;
+module.exports = detailSchema;
