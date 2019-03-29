1c2290eeebe186833aed4892efd2435919f59179
liuyu <myhome51up@126.com>
2019-03-29 14:03:58 +0800

    解决跨域问题

--- app.js
+++ app.js
@@ -12,6 +12,26 @@ const db = mongoose();
 
 const app = express();
 
+app.all('*', (req, res, next) => {
+    const {
+        origin,
+        Origin,
+        referer,
+        Referer
+    } = req.headers;
+    const allowOrigin = origin || Origin || referer || Referer || '*';
+    res.header("Access-Control-Allow-Origin", allowOrigin);
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Credentials", true); //可以带cookies
+    res.header("X-Powered-By", 'Express');
+    if (req.method == 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
